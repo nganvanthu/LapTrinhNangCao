09ddea0c6a30c086a9a0f66a5a63ab49cf4c6008
nganvanthu <80756305+nganvanthu@users.noreply.github.com>
2022-02-25 15:40:10 +0700

    second commit

---
+++ T1_BT6.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+main(){
+    string person1, person2, person3;
+    cin >> person1 >> person2 >>  person3;
+    cout << "Hi " << person3 << ", " <<  person2 << " and " <<  person1 << ".";
+    return 0;
+}
